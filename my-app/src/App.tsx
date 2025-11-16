a529aa54cddd5a414f2c7acef6bfabe3cd906262
Anusha <anukokala48@gmail.com>
2025-11-15 22:48:55 -0600

    Changes in set-up

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import WelcomeScreen from './components/WelcomeScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomeScreen from './components/WelcomeScreen.tsx';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Router>
         <Routes>
-          <Route path="/" element={<WelcomeScreen />} />
+          <Route path="/" element={<WelcomeScreen/>} />
         </Routes>
       </Router>
     </div>
   );
 }
+
